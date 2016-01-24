e05eaa59485f6842b626d93b1422dbf780a07157
Scott Kellum <scott@scottkellum.com>
2016-01-24 13:41:47 -0500

    re-write

--- modularscale.js
+++ modularscale.js
@@ -1,79 +1,74 @@
 // Values
-var minorSecond   = 1.067;
+var minorSecond   = 16/15;
 var majorSecond   = 1.125;
 var minorThird    = 1.2;
 var majorThird    = 1.25;
-var perfectFourth = 1.333;
-var augFourth    = 1.414;
+var perfectFourth = 4/3;
+var augFourth     = 1.414;
 var perfectFifth  = 1.5;
 var minorSixth    = 1.6;
-var goldenSection = 1.618;
-var majorSixth    = 1.667;
-var minorSeventh  = 1.778;
+var goldenSection = 1.61803398875;
+var majorSixth    = 5/3;
+var minorSeventh  = 16/9;
 var majorSeventh  = 1.875;
 var octave        = 2;
 var majorTenth    = 2.5;
-var majorEleventh = 2.667;
+var majorEleventh = 8/3;
 var majorTwelfth  = 3;
 var doubleOctave  = 4;
 
-console.log(msFunction(22));
-
-// Function
-function msFunction(v, base, ratio) {
-  return v;
+// Function settings
+var modularscale = {
+  base: 16,
+  ratio: 1.5,
+  med: {
+    ratio: 1.3
+  }
 };
 
-// @function ms-function($v: 0, $base: false, $ratio: false, $thread: false, $settings: $modularscale) {
+// Function
+function msFunction(v,settings) {
 
-//   // Parse settings
-//   $ms-settings: ms-settings($base,$ratio,$thread,$settings);
-//   $base: nth($ms-settings, 1);
-//   $ratio: nth($ms-settings, 2);
+  // Parse settings
+  // Write initial settings if undefined
+  if (settings === undefined) {
+    settings = modularscale;
+  }
+  // Initiate values
+  var base = settings.base;
+  var ratio = settings.ratio;
+  // Fill in the blanks with default values
+  if (ratio === undefined) {
+    ratio = modularscale.ratio;
+  }
+  if (base === undefined) {
+    base = modularscale.base;
+  }
 
-//   // Render target values from settings.
-//   @if unit($ratio) != '' {
-//     $ratio: ms-target($ratio,$base)
-//   }
+  // Fast calc if not multi stranded
+  if (!Array.isArray(base) || base.length === 1) {
+    return (Math.pow(ratio,v) * base);
+  }
 
-//   // Fast calc if not multi stranded
-//   @if(length($base) == 1) {
-//     @return ms-round-px(pow($ratio, $v) * $base);
-//   }
+  // Normalize bases
+  // Find the upper bounds for base values
+  var baseHigh = Math.pow(ratio,1) * base[0];
+  for (var i = 1; i < base.length; i++) {
+    // shift up if value too low
+    while (base[i] < base[0]) {
+      base[i] = Math.pow(ratio,1) * base[i];
+    }
+    // Shift down if too high
+    while (base[i] >= baseHigh) {
+      base[i] = Math.pow(ratio,-1) * base[i];
+    }
+  }
+  // Sort bases
+  base.sort();
 
-//   // Create new base array
-//   $ms-bases: nth($base,1);
+  // Figure out what base to use
+  var rBase = Math.round((v / base.length - Math.floor(v/base.length)) * base.length);
 
-//   // Normalize base values
-//   @for $i from 2 through length($base) {
-//     // initial base value
-//     $ms-base: nth($base,$i);
-//     // If the base is bigger than the main base
-//     @if($ms-base > nth($base,1)) {
-//       // divide the value until it aligns with main base.
-//       @while($ms-base > nth($base,1)) {
-//         $ms-base: $ms-base / $ratio;
-//       }
-//       $ms-base: $ms-base * $ratio;
-//     }
-//     // If the base is smaller than the main base.
-//     @elseif ($ms-base < nth($base,1)) {
-//       // pump up the value until it aligns with main base.
-//       @while $ms-base < nth($base,1) {
-//         $ms-base: $ms-base * $ratio;
-//       }
-//     }
-//     // Push into new array
-//     $ms-bases: append($ms-bases,$ms-base);
-//   }
-
-//   // Sort array from smallest to largest.
-//   $ms-bases: ms-sort($ms-bases);
-
-//   // Find step to use in calculation
-//   $vtep: floor($v / length($ms-bases));
-//   // Find base to use in calculation
-//   $ms-base: round(($v / length($ms-bases) - $vtep) * length($ms-bases)) + 1;
-
-//   @return ms-round-px(pow($ratio, $vtep) * nth($ms-bases,$ms-base));
-// }
+  // Return
+  return Math.pow(ratio,Math.floor(v/base.length)) * base[rBase];
+};
